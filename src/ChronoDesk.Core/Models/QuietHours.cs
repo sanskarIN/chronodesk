9cacd7c40bd3692a83bd3bee6805e1b96a650aa8
Sanskar <sanskarin@outlook.in>
2026-08-19 08:38:11 +0530

    feat: add quiet hours model

---
+++ src/ChronoDesk.Core/Models/QuietHours.cs
@@ -0,0 +1,22 @@
+namespace ChronoDesk.Core.Models;
+
+public sealed record QuietHours
+{
+    public bool Enabled { get; init; }
+
+    public TimeOnly Start { get; init; } = new(22, 0);
+
+    public TimeOnly End { get; init; } = new(7, 0);
+
+    public bool Contains(TimeOnly time)
+    {
+        if (!Enabled || Start == End)
+        {
+            return false;
+        }
+
+        return Start < End
+            ? time >= Start && time < End
+            : time >= Start || time < End;
+    }
+}
